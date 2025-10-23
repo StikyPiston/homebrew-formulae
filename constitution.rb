c97baa92c67674810e9d0f090e78dc40b0ff56f9
StikyPiston <StikyPiston@codeberg.org>
2025-10-23 12:37:22 +0200

    Fix syntax errors cuz im a dummy lol

--- constitution.rb
+++ constitution.rb
@@ -5,13 +5,13 @@ class Constitution < Formula
   sha256 "13d5bedc422c250fb35e28a76ac61b24d98337d828197ee39f82313490fca4df"
   license "MIT"
   
-  depends-on "ruby"
+  depends_on "ruby"
 
   def install
     system "gem install toml-rb"
     system "wget" "-P" "~/.local/bin" "https://codeberg.org/alternateved/dotfiles/raw/branch/main/scripts/.scripts/copr"
     bin.install "~/.local/bin/copr" "infrared-copr"
-    bin.install "constitution",
+    bin.install "constitution"
   end
 
   test do
