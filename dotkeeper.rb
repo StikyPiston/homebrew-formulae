265a2f9d054eccc538b02f37eb3066a14bf72547
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-11-20 18:27:13 +0200

    Update dotkeeper

--- dotkeeper.rb
+++ dotkeeper.rb
@@ -1,9 +1,9 @@
 class Dotkeeper < Formula
   desc "A dotfile management/symlink farm tool. Successor to rancher."
   homepage ""
-  url "https://codeberg.org/StikyPiston/dotkeeper/archive/201125c.zip"
-  sha256 "c92ffed5ddb9f12ccd2b4ea6380eb86090c96b7125f07db871c6db5f189c9709"
-  version "201125c"
+  url "https://codeberg.org/StikyPiston/dotkeeper/archive/201125c-hf1.zip"
+  sha256 "8cacd1dd27eb126b288bfc1e2122d9b18e48a374d839ca906ddbc99d4a3615ee"
+  version "201125c-hf1"
   license "MIT"
   
   depends_on "swift" => :build
